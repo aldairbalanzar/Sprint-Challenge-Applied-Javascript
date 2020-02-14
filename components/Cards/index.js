9d7dc0cc2302815952d4e377d5626cf39ed76b87
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-02-14 14:02:26 -0500

    updated files to mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,44 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container');
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+.then(res => {
+    let articles = res.data.articles;
+    createCard(articles);
+})
+.catch(res => {
+    console.log(`no cards`, res);
+})
+
+function createCard(obj) {
+    for(let prop in obj) {
+        obj[prop].forEach( item => {
+
+            let card = document.createElement('div'),
+                cardHeadline = document.createElement('div'),
+                cardAuthor = document.createElement('div'),
+                imgContainer = document.createElement('div'), 
+                img = document.createElement('img'),
+                byWho = document.createElement('span');
+
+            card.classList.add('card');
+            cardHeadline.classList.add('headline');
+            cardAuthor.classList.add('author');
+            imgContainer.classList.add('img-container');
+            
+            cardHeadline.textContent = item.headline;
+            cardAuthor.textContent = item.authorName;
+            img.setAttribute('src', item.authorPhoto);
+            byWho.textContent = `By ${item.authorName}`;
+
+            imgContainer.append(img);
+            cardAuthor.append(imgContainer);
+            console.log(card);
+            card.append(cardHeadline, cardAuthor, byWho);
+            return cardsContainer.append(card);
+        });
+    }
+}
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,24 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    let header = document.createElement('div'),
+    span1 = document.createElement('span'),
+    h1 = document.createElement('h1'),
+    span2 = document.createElement('span');
+
+    span1.classList.add('date');
+    header.classList.add('header');
+    span2.classList.add('temp');
+
+    span1.textContent = `SMARCH 28, 2019`;
+    h1.textContent = `Lambda Times`;
+    span2.textContent = `98`;
+
+    header.append(span1, h1, span2);
+    
+    return header
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.append(Header());
